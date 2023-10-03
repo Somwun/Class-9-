98836011a4aee2b79b1c8bedce0cf3aa123429ce
Christopher Staddon <c06staddon@gmail.com>
2023-10-03 10:17:22 -0400

    Add project files.

---
+++ Class (#9)/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class___9_
+{
+    class Student
+    {
+        private static Random generator = new Random();
+        private string _firstName;
+        private string _lastName;
+        private string _studentNum;
+        public Student(string firstName, string lastName)
+        {
+            this._firstName = firstName;
+            this._lastName = lastName;
+            this._studentNum = "";
+            for (int i = 0; i < 3; i++)
+            {
+                _studentNum += generator.Next(1, 10);
+            }
+            _studentNum += "555000";
+        }
+        public string StudentNum { get { return _studentNum; } }
+    }
+}
---
+++ Class (#9)/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Class___9_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*List<Student> students = new List<Student>();
+            string fName, lName;
+
+            students.Add(new Student("Foscar", "Grouch"));
+            students.Add(new Student("Alfred", "Barkley"));
+            students.Add(new Student("Steven", "Aldworth"));
+
+            Console.WriteLine("Testing setters and getters");
+            students[2].FirstName = "Steve";                //Test set property FirstName
+            Console.WriteLine(students[2].FirstName);       //Test get property FirstName
+            Console.WriteLine(students[2].StudentNumber);   //Test get property StudentNumber
+            students[1].LastName = "Hitchcock";             //Test set property LastName
+            Console.WriteLine(students[1].LastName);        //Test get property FirstName
+            Console.WriteLine(students[1].Email);           //Test get property get email
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Displays all names in list
+            Console.WriteLine("Students: ");
+            foreach (Student student in students)
+                Console.WriteLine(student); //ToString() implicitly called here
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Adds new students
+            Console.WriteLine("Adding new students");
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Please enter a first name:");
+                fName = Console.ReadLine();
+                Console.WriteLine("Please enter a last name:");
+                lName = Console.ReadLine();
+                students.Add(new Student(fName, lName));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Prints email addresses
+            Console.WriteLine("Email addresses:");
+            foreach (Student student in students)
+                Console.WriteLine(student.Email);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Sorts list using CompareTo()
+            Console.WriteLine("Sorted List:");
+            students.Sort();
+
+            //Prints sorted list
+            foreach (Student student in students)
+                Console.WriteLine(student);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to quit");
+            Console.ReadLine(); */
+            Student s = new Student("","");
+            Console.WriteLine(s.StudentNum);
+        }
+    }
+}
